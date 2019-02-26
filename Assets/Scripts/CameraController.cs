77718b219f9b0d3a8a426e36a2c83a92af9f2805
Andrew Pidcock <loopywolf@gmail.com>
2019-02-25 21:47:11 -0500

    Push to master after 2018.3.5
    Will you bloody DO IT ALREADY?

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,8 +17,11 @@ public class CameraController : MonoBehaviour {
 	void Update () {
         targetPosition = new Vector3(
             followTarget.transform.position.x,
+            //Mathf.RoundToInt(followTarget.transform.position.x), //attempt at fix - didn't work 2019-02-18
             followTarget.transform.position.y,
+            //Mathf.RoundToInt(followTarget.transform.position.y),
             transform.position.z );
+            //Mathf.RoundToInt(transform.position.z));
         transform.position = Vector3.Lerp(
             transform.position, 
             targetPosition, 
