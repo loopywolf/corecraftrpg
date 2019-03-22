1d2e921673f5ed2d0313396cdd67c862acbcabcc
Andrew Pidcock <loopywolf@gmail.com>
2019-03-21 22:13:46 -0400

    Sophisticated
    I get cursor, but i do it in scene manager

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
     public void SetupScene()
     {
         Debug.Log("SetupScene");
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
     }//F
 
 }//class
