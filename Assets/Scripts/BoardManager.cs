38b84220d4d3a7eb7378f1f22d699d1ef08ddce9
Andrew Pidcock <loopywolf@gmail.com>
2019-03-21 22:10:15 -0400

    I got cursor
    I did it!

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    public Texture2D cursorTexture;
-    public CursorMode cursorMode = CursorMode.Auto;
-    public Vector2 hotSpot = Vector2.zero;
-
-    // Update is called once per frame
     public void SetupScene()
     {
         Debug.Log("SetupScene");
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-        Debug.Log("I set cursor?");
     }//F
 
 }//class
---
+++ Assets/Scripts/ChangeCursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCursor : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Oh no! Steven!");
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }//Start
+
+}//class
